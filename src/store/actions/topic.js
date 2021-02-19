aa873ba918ff3f9bad339dbc9eeea74efc31d299
AustynT <Austyntravis9@gmail.com>
2021-02-19 15:39:17 -0700

    updated topic actions

--- src/store/actions/topic.js
+++ src/store/actions/topic.js
@@ -1,9 +1,39 @@
 import * as actionTypes from './actionsTypes';
 
 
+export const getTopic = () => {
+    return {
+        type: actionTypes.GET_TOPICS
+    }
+}
+
 export const createTopic = (title) => {
     return {
         type: actionTypes.CREATE_TOPIC,
-        title: title
+        payload: 
+        {
+            title: title
+        } 
+    }
+}
+
+export const updateTopic = (id, title) => {
+    return {
+        type: actionTypes.UPDATE_TOPIC,
+        payload: 
+        {
+            id: id,
+            title: title
+        } 
+    }
+}
+
+export const deleteTopic = (id) => {
+    return {
+        type: actionTypes.DELETE_TOPIC,
+        payload: 
+        {
+            id: id
+        }
     }
 }
