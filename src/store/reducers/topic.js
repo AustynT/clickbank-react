7a53ee59c5327879bc73910957cc71a9fec553dc
AustynT <Austyntravis9@gmail.com>
2021-02-18 19:23:57 -0700

    updated topic reducer

--- src/store/reducers/topic.js
+++ src/store/reducers/topic.js
@@ -4,6 +4,23 @@ const initialState = {
 
 const topicReducer = (state = initialState, action) => 
 {
+    switch (action.type) {
+        case 'GET_TOPICS':
+            return {
+                topics: []
+            }
+        case 'UPDATE_TOPIC':
+            return {
+                topics: []
+            }
+        case 'DELETE_TOPIC':
+            return {
+                topics: []
+            }
+    
+        default:
+            break;
+    }
     return state;
 };
 
